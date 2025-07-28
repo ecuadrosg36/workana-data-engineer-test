64fd7b82e93c95c9961ec68a131351b043e185f0
Enmanuel Cuadros Gomez <ecuadrosg36@gmail.com>
2025-07-27 23:11:35 -0500

    se agrega docker y ejecucion del dag

--- scripts/download_csv.py
+++ scripts/download_csv.py
@@ -2,30 +2,27 @@ import requests
 import os
 
 def download_csv(url: str, output_path: str):
-    print(f"🔽 Descargando desde: {url}")
-    response = requests.get(url, stream=True)
+    print("🔽 Iniciando descarga del archivo CSV...")
+    print(f"🔗 URL: {url}")
+    print(f"📥 Guardando en: {output_path}")
 
-    # Verifica código de estado
+    headers = {
+        "User-Agent": "Mozilla/5.0"  # Soluciona problemas con Dropbox
+    }
+
+    response = requests.get(url, stream=True, headers=headers)
+
     if response.status_code != 200:
         raise Exception(f"❌ Error al descargar archivo. Código HTTP: {response.status_code}")
 
-    # Verifica tipo de contenido
     content_type = response.headers.get("Content-Type", "")
     if "text/html" in content_type:
         raise Exception("❌ Error: El archivo descargado parece ser HTML (¿URL sin 'dl=1'?)")
 
-    # Guarda el archivo si todo está bien
     os.makedirs(os.path.dirname(output_path), exist_ok=True)
     with open(output_path, "wb") as f:
         for chunk in response.iter_content(chunk_size=8192):
             f.write(chunk)
 
-    file_size = os.path.getsize(output_path)
-    print(f"✅ Archivo guardado correctamente en {output_path} ({file_size:,} bytes)")
-
-
-# Uso manual (debug local)
-if __name__ == "__main__":
-    url = "https://www.dropbox.com/scl/fi/9s8zptquw3urvhsctmgya/sample_transactions.csv?rlkey=8s2wyvvjcm96c6ctopvwnskwj&dl=1"
-    output_path = "data/sample_transactions.csv"
-    download_csv(url, output_path)
+    size = os.path.getsize(output_path)
+    print(f"✅ Archivo descargado correctamente en {output_path} ({size:,} bytes)")
